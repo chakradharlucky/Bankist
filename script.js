5712e1e25a57b9ac9d3c5bbe8c7095dd14787575
chakradharlucky <chakradharlakinana@gmail.com>
2023-11-29 17:02:39 +0530

    Respond Navigation

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const account4 = {
 
 // Account array
 let accounts = [account1,account2,account3,account4]
-console.log(accounts)
+// console.log(accounts)
 
 // Select elements
 //Select navigation bar elements
@@ -45,17 +45,30 @@ const transfer_money_userName = document.querySelector("#transfer_money_userName
 const transfer_money_amount = document.querySelector("#transfer_money_amount")
 const transfer_money_button = document.querySelector("#transfer_money_button")
 // Select Request loan elements
-const request_loan_amount = document.querySelecter("#request_loan_amount")
+const request_loan_amount = document.querySelector("#request_loan_amount")
 const request_loan_button = document.querySelector("#request_loan_button")
 // Select Close account elements
 const close_account_user_name = document.querySelector("#close_account_user_name");
 const close_account_pin = document.querySelector("#close_account_pin")
 const close_account_button = document.querySelector("#close_account_button")
+const balance_lable = document.querySelector("#balance")
 
-
+//Functions 
+//Create user name
 function createUserName(accounts){
   accounts.forEach(account=>account.userName = account.owner.toLowerCase().split(" ").map(splitname=> splitname[0]).join(""))
 }
-
-//Create user name
 createUserName(accounts)
+
+let  balance = (account) => account.reduce((sum=0,movment)=> sum+movment)
+
+// console.log(balance([4300, 10000, 7000, 500, 900]));
+let currentUser;
+login_button.addEventListener('click',function(){
+  currentUser = accounts.find((account) => account.userName===login_username.value);
+  if(currentUser.pin === Number(login_password.value)){
+    app.style = "opacity: 100";
+    greet.textContent = `Wellcome,${currentUser.owner.split(" ")[1]}`
+    balance_lable.textContent = balance(currentUser.movements)
+  }
+})
